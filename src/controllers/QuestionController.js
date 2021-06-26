fac895faf2c4032476f62d60a8d1316942fa7d01
ThorH <thor.haubert@hotmail.com>
2021-06-26 19:31:03 -0300

    Envio das perguntas para o banco de dados

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,4 +1,9 @@
+const { create } = require("./RoomController")
+const Database = require('../db/config')
+
 module.exports = {
+
+
     index(req, res){
         const roomId = req.params.room
         const questionId = req.params.question
@@ -7,5 +12,23 @@ module.exports = {
 
         console.log(`room = ${roomId}, questionId = ${questionId}, action = ${action}, password = ${password}`)
 
+    },
+
+    async create(req, res){
+        const db = await Database()
+        const question = req.body.question
+        const roomId = req.params.room
+
+        await db.run(`INSERT INTO questions(
+            title,
+            room,
+            read
+        )VALUES(
+            "${question}",
+            ${roomId},
+            0
+        )`)
+
+        res.redirect(`/room/${roomId}`)
     }
 }
