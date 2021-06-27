f24290d221949266a374a8d1027e43de176efa02
ThorH <thor.haubert@hotmail.com>
2021-06-27 14:10:50 -0300

    Trazendo todas questões das salas, entrar na salas criadas e aviso que não tem questões na sala

--- src/route.js
+++ src/route.js
@@ -10,9 +10,10 @@ route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'
 
 route.post('/create-room', RoomController.create)
 route.get('/room/:room', RoomController.open)
+route.post('/enterroom', RoomController.enter)
 
 // Formato que o formulario de dentro da modal tem que passar a informação
 route.post('/question/create/:room', QuestionController.create)
-route.post('question/:room/:question/:action', QuestionController.index)
+route.post('/question/:room/:question/:action', QuestionController.index)
 
 module.exports = route
